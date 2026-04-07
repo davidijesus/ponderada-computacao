932ff76d30f4415ac2f5545fff46e4c98a477d2e
davidijesus <davinasc.dejesus@gmail.com>
2026-04-07 08:42:08 -0300

    feat: ajuste de connection.go

--- src/worker/queue/connection.go
+++ src/worker/queue/connection.go
@@ -1,6 +1,9 @@
 package queue
 
 import (
+	"log"
+	"time"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -10,7 +13,15 @@ var ch *amqp.Channel
 func InitBroker() error {
 	var err error
 
-	conn, err = amqp.Dial("amqp://guest:guest@broker:5672/")
+	for i := 1; i <= 10; i++ {
+		conn, err = amqp.Dial("amqp://guest:guest@broker:5672/")
+		if err == nil {
+			break
+		}
+		log.Printf("Tentativa %d/10: broker indisponível, aguardando 5s...\n", i)
+		time.Sleep(5 * time.Second)
+	}
+
 	if err != nil {
 		return err
 	}
@@ -20,6 +31,7 @@ func InitBroker() error {
 		return err
 	}
 
+	log.Println("Conectado ao broker com sucesso")
 	return nil
 }
 
